1fb9a4ecc20abfc4588f8af65bb2888620681155
Sharath Kumar T P <sharathkumartp.1si18ee049@gmail.com>
2023-02-05 21:35:07 +0530

    restaurant

---
+++ Restaurant/src/main/java/com/sharathgeekster/Restaurant/controller/RestaurantController.java
@@ -0,0 +1,47 @@
+package com.sharathgeekster.Restaurant.controller;
+
+
+import com.sharathgeekster.Restaurant.model.Restaurant;
+import com.sharathgeekster.Restaurant.service.RestaurantService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/restaurant-app")
+public class RestaurantController {
+
+    private final RestaurantService restaurantService;
+
+    public RestaurantController(RestaurantService restaurantService){
+        this.restaurantService = restaurantService;
+    }
+
+    //http://localhost:8080/api/v1/restaurant-app/add-restaurant
+
+    @PostMapping("/add-restaurant")
+    public void addRestaurant(@RequestBody Restaurant restaurant){
+        restaurantService.addRestaurant(restaurant);
+    }
+
+    @GetMapping("find-all")
+    public List<Restaurant> findAllRestaurants(){
+        return restaurantService.findAll();
+    }
+
+    @GetMapping("/find-restaurant/name/{name}")
+    public Restaurant findByName(@PathVariable String name){
+        return restaurantService.findByName(name);
+    }
+
+    @DeleteMapping("/delete-restaurant/name/{name}")
+    public void deleteRestaurant(@PathVariable String name){
+        restaurantService.deleteRestaurant(name);
+    }
+
+    @PutMapping("update-restaurant/name/{name}")
+    public void updateRestaurant(@PathVariable String name , @RequestBody Restaurant restaurant){
+        restaurantService.updateRestaurant(name,restaurant);
+    }
+
+}
---
+++ Restaurant/src/main/java/com/sharathgeekster/Restaurant/service/RestaurantService.java
@@ -0,0 +1,51 @@
+package com.sharathgeekster.Restaurant.service;
+
+
+import com.sharathgeekster.Restaurant.model.Restaurant;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class RestaurantService {
+    private static List<Restaurant> restaurants = new ArrayList<>();
+
+    static{
+        restaurants.add(new Restaurant("Beijing", "China Town" , 784949456,"Chinese",100,4.6,1000));
+    }
+
+    public void addRestaurant(Restaurant restaurant){
+        restaurants.add(restaurant);
+    }
+    public List<Restaurant> findAll(){
+        return restaurants;
+    }
+
+    public Restaurant findByName(String name){
+        for(Restaurant search : restaurants){
+            if(search.getName().equals(name)) return search;
+        }
+        return null;
+    }
+
+    public void deleteRestaurant(String name){
+        for(int idx = 0 ; idx < restaurants.size() ; idx++){
+            if(restaurants.get(idx).getName().equals(name)) restaurants.remove(idx);
+        }
+    }
+
+    public void updateRestaurant(String name , Restaurant newRestaurant){
+        Restaurant search = findByName(name);
+
+        search.setName(newRestaurant.getName());
+        search.setLocation(newRestaurant.getLocation());
+        search.setNumber(newRestaurant.getNumber());
+        search.setCuisine(newRestaurant.getCuisine());
+        search.setBudget(newRestaurant.getBudget());
+        search.setTotalStaff(newRestaurant.getTotalStaff());
+        search.setRating(newRestaurant.getRating());
+    }
+
+
+}
