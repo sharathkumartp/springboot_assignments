5c57b7b418c1639ce208b9b3e5f3d0eb29d87214
Sharath Kumar T P <sharathkumartp.1si18ee049@gmail.com>
2023-02-27 00:08:08 +0530

    update changes

--- todo/src/main/java/com/example/todo/model/Todo.java
+++ todo/src/main/java/com/example/todo/model/Todo.java
@@ -1,53 +1,23 @@
 package com.example.todo.model;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class Todo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String title;
     private boolean status;
-
-    Todo(){
-
-    }
-
-
-     Todo(int id, String title, boolean status) {
-        this.id = id;
-        this.title = title;
-        this.status = status;
-    }
-
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public boolean isStatus() {
-        return status;
-    }
-
-    public void setStatus(boolean status) {
-        this.status = status;
-    }
-
-
-    @Override
-    public String toString() {
-        return "Todo{" +
-                "id=" + id +
-                ", title='" + title + '\'' +
-                ", status=" + status +
-                '}';
-    }
 }
--- todo/src/main/java/com/example/todo/service/TodoService.java
+++ todo/src/main/java/com/example/todo/service/TodoService.java
@@ -1,13 +1,52 @@
 package com.example.todo.service;
 
 import com.example.todo.model.Todo;
+import com.example.todo.repository.TodoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Service
-public class TodoService {
-    private List<Todo> todo=new ArrayList<>();
+public class TodoService implements  ITodoService {
 
+    private static List<Todo> todos=new ArrayList<>(); // In-memory database
+
+    @Autowired
+    TodoRepository todoRepository;
+
+    @Override
+    public List<Todo> findAll() { // Business Logic
+        return todoRepository.findAll();
+    }
+
+    @Override
+    public Todo findById(int id) { // business Logic
+        return todoRepository.findById(id).get();
+    }
+
+    @Override
+    public void addTodo(Todo todo) {
+        todoRepository.save(todo);
+    }
+
+    @Override
+    public void deleteTodo(int id) {
+        todoRepository.deleteById(id);
+    }
+
+    @Override
+    public void updateTodo(int id,Todo newTodo) {
+        //step 1: find todo to be update
+        //Step 2: update todo
+
+        Todo todo=todoRepository.findById(id).get();
+
+        todo.setId(newTodo.getId());
+        todo.setTitle(newTodo.getTitle());
+        todo.setStatus(newTodo.isStatus());
+
+        todoRepository.save(todo);
+    }
 }
